49d828635184d46ae02808d6b18b19b4f0bc90ad
graphigen <cagatayyumlu@gmail.com>
2025-08-09 00:09:46 +0300

    Add EXAMPLE_NAME environment variable for Vercel integration

---
+++ components/ui/slot.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+
+export interface SlotProps extends React.HTMLAttributes<HTMLElement> {
+  asChild?: boolean
+}
+
+const SlotComponent = React.forwardRef<HTMLElement, SlotProps>(
+  ({ asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "div"
+    return <Comp ref={ref} {...props} />
+  }
+)
+SlotComponent.displayName = "Slot"
+
+export { SlotComponent as Slot }
+
